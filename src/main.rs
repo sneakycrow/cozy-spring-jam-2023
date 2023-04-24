d4e5d8e848f2415d0075a49ffafb63238c23cc33
sneakycrow <zach@sneakycrow.dev>
2023-04-24 16:53:28 -0700

    add wasm output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use bevy::prelude::*;
+use bevy_svg::prelude::*;
 
 mod components;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_startup_system(setup)
         .add_plugins(DefaultPlugins)
         .add_plugin(SvgPlugin)
         .run();
 }
 
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    let svg = asset_server.load("logo_v2.svg");
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Svg2dBundle {
+        svg,
+        origin: Origin::Center, // Origin::TopLeft is the default
+        ..Default::default()
+    });
+}
