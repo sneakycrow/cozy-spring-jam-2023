b0109a28860b39572440deca14f795cff73a5d18
sneakycrow <zach@sneakycrow.dev>
2023-04-24 16:53:19 -0700

    remove placeholder plugins

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use bevy::DefaultPlugins;
 use bevy::prelude::App;
 
 mod components;
-mod plugins;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(plugins::HelloPlugin)
+        .add_plugin(SvgPlugin)
         .run();
 }
 
